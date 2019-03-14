be0f95111996ba073e46b59eaea6ed96787962a2
ssrzz <ssrzz@pm.me>
2019-03-14 13:37:38 +0800

    637 blocked user added

--- review.py
+++ review.py
@@ -9,18 +9,20 @@ import urllib
 def get_keysNtokens(keyFile):
         return [str(v) for (k, v) in sorted(json.load(open(keyFile, 'r')).items())]
 
-# def getScreen
 auth = "/usr/local/info/fayder.json"
 t = Twitter(auth=OAuth(*get_keysNtokens(auth)))
+breakpoint = True
+posted = json.load(open('posted.json', 'r'))
+pprint(posted)
 
-with open('list0314.csv', 'r') as f:
-	for ln in f.readlines()[:10]:
+with open('blocklist.csv', 'r') as f:
+	for ln in f.readlines():
 		uid = ln.strip()
-		# u = t.get_user(uid)
-		x = t.statuses.user_timeline(_id=uid, count=1)
-		# pprint(x)
+		posted[uid] = True
 		try:
+			x = t.statuses.user_timeline(_id=uid, count=1)			
 			print("|{}|{}|".format(uid, x[0]['user']['screen_name']))
 		except Exception as e:
 			print("|{}|{}|".format(uid, 'UNKNOWN'))
+	json.dump(posted, open('posted.json', 'w'), indent=2)
 
